87cf10e08ed5c79f0874262a81eb0623c738bc4b
Igor <sergienko339@gmail.com>
2021-08-16 00:54:29 +0300

    basic clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Link to="/">main</Link>
+          <Link to="/people">peole</Link>
+          <Link to="/starships">starships</Link>
+          <Link to="/planets">planets</Link>
+        </header>
+
+        <Switch>
+          <Route path="/" exact>
+            <h1>Main</h1>
+          </Route>
+          <Route path="/people">
+            <h1>People</h1>
+          </Route>
+          <Route path="/starships">
+            <h1>Starships</h1>
+          </Route>
+          <Route path="/planets">
+            <h1>Planets</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
